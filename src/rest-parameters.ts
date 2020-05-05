4e7de403eec824b40f513624705e595f02b23dd6
大西雄貴 <>
2020-05-05 18:00:04 +0900

    rest patameter

---
+++ src/rest-parameters.ts
@@ -0,0 +1,16 @@
+export {};
+
+// Restパラメーター(...引数名)
+
+const reducer = (accumulator: number, currentValue: number): number => {
+  console.log({ accumulator, currentValue });
+  return accumulator + currentValue;
+};
+
+const sum: (...values: number[]) => number = (...values: number[]): number => {
+  return values.reduce(reducer);
+};
+
+console.log(sum(1, 2, 3, 4, 5));
+
+// [1, 2, 3, 4].reduce(reducer);
