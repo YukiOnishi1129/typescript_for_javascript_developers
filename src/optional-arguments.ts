1af957544d80ba025ac894761125f264b47b9f07
大西雄貴 <>
2020-05-05 17:48:22 +0900

    optional-parameter

---
+++ src/optional-arguments.ts
@@ -0,0 +1,23 @@
+export {};
+
+//  オプショナルなパラメータ(必須ではないパラメータ)
+// ?を書くと、オプショナルな引数になる
+let bmi: (height: number, weight: number, printable?: boolean) => number = (
+  height: number,
+  weight: number,
+  printable?: boolean
+): number => {
+  const bmi: number = weight / (height * height);
+
+  if (printable) {
+    console.log({ bmi });
+  }
+  return bmi;
+};
+
+// bmi(1.78, 86, false);
+
+// bmi(身長, 体重, console.log()で出力するかどうか)
+// bmi(1.78, 86, true);
+// bmi(1.78, 86, false);
+// bmi(1.78, 86);
